db6516b84483c5237e160c2f80e94d2c8e9f127f
isnoopytokky <tokpower2@gmail.com>
2013-10-25 12:04:31 +0700

    test commit

---
+++ test/test/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  test
+//
+//  Created by Kanokwan on 10/25/2556 BE.
+//  Copyright (c) 2556 Kanokwan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
